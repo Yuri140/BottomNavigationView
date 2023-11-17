1db8c8e37ad5e71025ce559228c147431caef124
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-11-16 22:00:22 -0300

    Return view nas classes java

--- app/src/main/java/br/com/etecia/myapp/FlightsFragment.java
+++ app/src/main/java/br/com/etecia/myapp/FlightsFragment.java
@@ -17,6 +17,7 @@ public class FlightsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flights, container, false);
+        View view = inflater.inflate(R.layout.fragment_flights, container, false);
+        return view;
     }
 }
